b04d7ad0557e8d9127a71a17bb3c6a54c795dab4
Ivan Ladanov <ladanov1@gmail.com>
2021-05-01 12:47:42 +0300

    PRO DIPLOM V1.0

---
+++ pro_diplom/dblogic.py
@@ -0,0 +1,57 @@
+import sqlalchemy
+
+class VkDB:
+
+    def __init__(self, dbuser='postgres', dbpass='zxcvbn123', ip='localhost', port='5432', dbname='diplom'):
+        db = f'postgresql://{dbuser}:{dbpass}@{ip}:{port}/{dbname}'
+        engine = sqlalchemy.create_engine(db)
+        self.connection = engine.connect()
+
+        ss = self.create_tbl()
+
+    def create_tbl(self):
+        ss = self.connection.execute("""
+        create table if not exists VkIdUsers3 (
+        	Id serial primary key,
+        	VkId integer not null,
+        	VkUserId integer not null
+        );
+        """)
+
+        return ss
+
+    def get_users_list(self, vkid):
+        '''Возвращает массив с ID пользователей, которые уже показывались пользователю'''
+        ss = self.connection.execute(f"""
+        SELECT * from vkidusers3
+        WHERE vkid = {vkid}
+        """).fetchall()
+
+        users_list = list()
+        for element in ss:
+            users_list.append(element[2])
+
+        return list(set(users_list))
+
+    def insert_users_list(self, vkid, *args):
+        '''Добавляет в базу список людей, которые уже показывались данном vkid'''
+        for num in args:
+            ss = self.connection.execute(f"""
+            INSERT INTO VkIdUsers3(VkId, VkUserId)
+            VALUES
+            ({vkid}, {num});
+                                        """)
+
+    def clear(self):
+        ss = self.connection.execute("""
+            DROP TABLE IF EXISTS vkidusers3
+        """)
+
+        ss = self.create_tbl()
+
+        return ss
+
+
+
+
+
